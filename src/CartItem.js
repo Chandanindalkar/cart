12ec745154a82948799c28798f8a01de39b0e245
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-07-07 19:13:15 +0530

    buttons work now except for delete

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,42 @@ class CartItem extends React.Component{
         }
     }
 
+    increaseQuantity = () => {
+        // this.setState({
+        //     qty: this.state.qty + 1,
+        //     title: 'new'            
+        // })
+
+        // if prev state is required then used this
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1     
+            }
+        })
+
+    }
+    
+    decreaseQuantity = () => {
+        // this.setState({
+        //     qty: this.state.qty - 1,
+        // })
+        this.setState((prevState) => {
+            if (prevState.qty == 1) {
+                return {
+                    qty: prevState.qty     
+                }
+            } else {
+                return {
+                    qty: prevState.qty - 1     
+                }
+            } 
+        })
+    }
+
+    deleteItem = () => {
+        
+    }
+
     // for a class componenet to be a react component, we use render
     render() { 
         const {price, title, qty} = this.state; // destructuring
@@ -27,9 +63,24 @@ class CartItem extends React.Component{
                     <div>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons Here*/}
-                        <img alt="increase" className="action-icons" src="https://www.svgrepo.com/show/507398/plus-circle.svg"/>
-                        <img alt="decrease" className="action-icons" src="https://www.svgrepo.com/show/507369/minus-circle.svg"/>
-                        <img alt="delete" className="action-icons" src="https://www.svgrepo.com/show/500534/delete-filled.svg"/>
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://www.svgrepo.com/show/507398/plus-circle.svg"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://www.svgrepo.com/show/507369/minus-circle.svg"
+                            onClick={this.decreaseQuantity}
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://www.svgrepo.com/show/500534/delete-filled.svg"
+                            onClick={this.deleteItem}
+                        />
                     </div>
                 </div>
             </div>
