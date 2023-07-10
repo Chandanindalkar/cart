5b84e31149bc6cbd8a9d8ea7bc0f5dd22e9cb1a0
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-07-10 11:50:13 +0530

    fixed increase & decrease qty, moved it to Cart

--- src/Cart.js
+++ src/Cart.js
@@ -4,35 +4,64 @@ import "./index.css"
 
 class Cart extends React.Component {
     constructor() {
-        super();
-        this.state = {
-            products: [
-                {
-                  price: 99,
-                  title: 'Watch',
-                  qty: 1,
-                  img: '',
-                  id: 1
-                },
-                {
-                  price: 999,
-                  title: 'Mobile Phone',
-                  qty: 10,
-                  img: '',
-                  id: 2
-                },
-                {
-                  price: 999,
-                  title: 'Laptop',
-                  qty: 4,
-                  img: '',
-                  id: 3
-                }
-            ]
-        }
+      super();
+      this.state = {
+        products: [
+          {
+            price: 99,
+            title: 'Watch',
+            qty: 1,
+            img: '',
+            id: 1
+          },
+          {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 10,
+            img: '',
+            id: 2
+          },
+          {
+            price: 999,
+            title: 'Laptop',
+            qty: 4,
+            img: '',
+            id: 3
+          }
+        ]
+      }
+    }
+    handleIncreaseQuantity = (product) => {
+      console.log('This increases the qty of product ', product);
+      const { products } = this.state;
+      // console.log(products)
+      const index = products.indexOf(product);
+  
+      products[index].qty += 1;
+  
+      this.setState({
+        products
+      })
+    }
+
+    handleDecreaseQuantity = (product) => {
+      console.log('This decreases the qty of product ', product);
+      const { products } = this.state;
+      const index = products.indexOf(product);
+
+      if(product.qty == 1){
+        return;
+      } else {
+        products[index].qty -= 1;
+    
+        this.setState({
+          products
+        })
+      }
     }
   render () {
     const { products } = this.state;
+    // console.log('this is products', products)
     return (
       <div className="cart">
         {products.map((product) => { 
@@ -40,6 +69,8 @@ class Cart extends React.Component {
             <CartItem
               product={product}
               key={product.id}
+              onIncreaseQuantity={this.handleIncreaseQuantity}
+              onDecreaseQuantity={this.handleDecreaseQuantity}
             />
           )
         })}
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,37 +3,6 @@ import "./index.css"
 
 // class based componenet
 class CartItem extends React.Component{
-    increaseQuantity = () => {
-        // this.setState({
-        //     qty: this.state.qty + 1,
-        //     title: 'new'            
-        // })
-
-        // if prev state is required then used this
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1     
-            }
-        })
-
-    }
-    
-    decreaseQuantity = () => {
-        // this.setState({
-        //     qty: this.state.qty - 1,
-        // })
-        this.setState((prevState) => {
-            if (prevState.qty == 1) {
-                return {
-                    qty: prevState.qty     
-                }
-            } else {
-                return {
-                    qty: prevState.qty - 1     
-                }
-            } 
-        })
-    }
 
     deleteItem = () => {
         
@@ -59,13 +28,13 @@ class CartItem extends React.Component{
                             alt="increase" 
                             className="action-icons" 
                             src="https://www.svgrepo.com/show/507398/plus-circle.svg"
-                            onClick={this.increaseQuantity}
+                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
                         />
                         <img
                             alt="decrease" 
                             className="action-icons" 
                             src="https://www.svgrepo.com/show/507369/minus-circle.svg"
-                            onClick={this.decreaseQuantity}
+                            onClick={() => this.props.onDecreaseQuantity(this.props.product)}
                         />
                         <img 
                             alt="delete" 
